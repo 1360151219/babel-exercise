c6b64d1a7d3c8410b18ada943ecbc6c6a05111b7
ZJX <1360151219@qq.com>
2022-06-21 16:38:55 +0800

    [feat]globally use function in JS-interpreter

--- Js-Interpret/index.js
+++ Js-Interpret/index.js
@@ -1,11 +1,40 @@
 import parser from "@babel/parser"
 import { codeFrameColumns } from "@babel/code-frame"
-
-const code = `const a=1+2`
+import chalk from "chalk"
+const code = `
+const a=1+2
+console.log(a)
+console.error(a)
+console.warn(a)
+`
 const ast = parser.parse(code, {
     sourceType: "unambiguous"
 })
+class Scope {
+    constructor(parentScope) {
+        this.parent = parentScope
+        this.declarations = []
+    }
+    set(name, val) {
+        this.declarations[name] = val
+    }
+    getLocal(name) {
+        return this.declarations[name]
+    }
+    get(name) {
 
+        let res = this.getLocal(name)
+        if (!res && this.parent) {
+            // 递归向上搜索
+
+            res = this.parent.get(name)
+        }
+        return res
+    }
+    has(name) {
+        return !!this.getLocal(name)
+    }
+}
 const astInterpreters = {
     Program: (node, scope) => {
         node.body.forEach((child) => {
@@ -20,17 +49,33 @@ const astInterpreters = {
     },
     VariableDeclarator: (node, scope) => {
         let id = evaluate(node.id)
-
         if (scope.has(id)) {
             throw Error('duplicate declare variable：' + declareName);
         } else {
             let init = evaluate(node.init)
             scope.set(id, init)
         }
+    },
+    ExpressionStatement: (node, scope) => {
+        evaluate(node.expression, scope)
+    },
+    CallExpression: (node, scope) => {
+        let fn = evaluate(node.callee, scope)
 
+        let args = node.arguments.map((item) => {
+            if (item.type === 'Identifier') return scope.get(item.name)
+            return evaluate(item, scope)
+        })
+        if (node.callee.type === 'MemberExpression') {
+            const obj = evaluate(node.callee.object)
+            return fn.apply(obj, args)
+        } else fn.apply(null, args)
 
+    },
+    MemberExpression: (node, scope) => {
+        const obj = scope.get(evaluate(node.object))
 
-
+        return obj[evaluate(node.property)]
     },
     Identifier: (node, scope) => {
         return node.name
@@ -72,6 +117,17 @@ function evaluate(node, scope) {
 
     }
 }
-const globalScope = new Map()
+const globalScope = new Scope()
+globalScope.set('console', {
+    log: function (...args) {
+        console.log(chalk.green(...args));
+    },
+    error: function (...args) {
+        console.error(chalk.red(...args));
+    },
+    warn: function (...args) {
+        console.warn(chalk.yellow(...args));
+    }
+})
 evaluate(ast.program, globalScope)
 console.log(globalScope);
